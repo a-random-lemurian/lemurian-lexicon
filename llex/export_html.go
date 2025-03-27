a0b40daecbbbd594f6c957c77e513c469f40e31b
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2025-03-27 15:37:44 +0800

    feat(html): Include language name in <title>

--- llex/export_html.go
+++ llex/export_html.go
@@ -13,7 +13,7 @@ var HtmlTemplate = `
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Dictionary</title>
+    <title>{{.LanguageName}} Dictionary</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <style>
         .dictionary ol {
